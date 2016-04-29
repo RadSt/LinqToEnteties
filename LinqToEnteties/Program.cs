64752249cfa1f84635e7fd6d19dc3d53dc716203
Radik <desan1986@gmail.com>
2016-04-29 16:57:40 +0300

    Create List LIst entity

--- LinqToEnteties/Program.cs
+++ LinqToEnteties/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace LinqToEnteties
 {
@@ -9,20 +8,102 @@ namespace LinqToEnteties
     {
         static void Main(string[] args)
         {
-            string[] cars =
-            {
-                "Mercedes", "Porshe", "Toyota", "VAZ", "OKA", "KAMAZ", "Nissan", "Reno", "KIA",
-                "Folkswagen"
-            };
+            List<Car> cars = CarsInitializer.FillCars(new Car());
 
-            var filteredCars = cars.Where((c, i) => (i & 1) == 1);
-
-            foreach (var car in filteredCars)
+            var carsFiltered = cars.Select(x => new
             {
+                x.Cars,
+                x.Name
+            })
+                .Select(c => new
+                {
+                    c.Name
+                });
+
+            foreach (var car in carsFiltered)
+            {
+                foreach (var carList in car)
+                {
+                    
+                }
                 Console.WriteLine(car);
             }
 
             Console.ReadKey();
+        }
+
+        public class Car
+        {
+            public string Name { get; set; }
+            public List<Car> Cars { get; set; }
+        }
+
+        public static class CarsInitializer 
+        {
+            public static List<Car> FillCars(Car car)
+            {
+                List<Car> cars = new List<Car>(); 
+                car = new Car
+                {
+                    Name = "1",
+
+                    Cars = new List<Car>
+                    {
+                        new Car()
+                        {
+                            Name = "1",
+                            Cars = new List<Car>()
+                        },
+                        new Car()
+                        {
+                            Name = "2",
+                            Cars = new List<Car>()
+                        },
+                        new Car()
+                        {
+                            Name = "3",
+                            Cars = new List<Car>()
+                        },
+                        new Car()
+                        {
+                            Name = "4",
+                            Cars = new List<Car>()
+                        },
+                        new Car()
+                        {
+                            Name = "5",
+                            Cars = new List<Car>()
+                        },
+                        new Car()
+                        {
+                            Name = "6",
+                            Cars = new List<Car>()
+                        },
+                        new Car()
+                        {
+                            Name = "7",
+                            Cars = new List<Car>()
+                        },
+                        new Car()
+                        {
+                            Name = "8",
+                            Cars = new List<Car>()
+                        },
+                        new Car()
+                        {
+                            Name = "9",
+                            Cars = new List<Car>()
+                        },
+                        new Car()
+                        {
+                            Name = "10",
+                            Cars = new List<Car>()
+                        }
+                    }
+                };
+                cars.Add(car);
+                return cars;
+            }
         }
     }
 }
