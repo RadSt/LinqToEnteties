e97d84f2646318814f7fe6a3277ed8cfc3931e4a
Radik <desan1986@gmail.com>
2016-05-06 10:48:43 +0300

    Test commit IIS Err

--- EF7EntityApp/src/EF7EntityApp/gulpfile.js
+++ EF7EntityApp/src/EF7EntityApp/gulpfile.js
@@ -1,22 +1,27 @@
-﻿/// <binding AfterBuild='moveToLibs' />
+﻿/// <binding AfterBuild='angular2' />
 var gulp = require('gulp');
+var paths = {};
+paths.webroot = "wwwroot/";
 
-gulp.task('moveToLibs', function (done) {
-    gulp.src([
-      'node_modules/angular2/bundles/js',
-      'node_modules/angular2/bundles/angular2.*.js*',
-      'node_modules/angular2/bundles/angular2-polyfills.js',
-      'node_modules/angular2/bundles/http.*.js*',
-      'node_modules/angular2/bundles/router.*.js*',
-      'node_modules/es6-shim/es6-shim.min.js*',
-      'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
-      'node_modules/systemjs/dist/*.*',
-      'node_modules/jquery/dist/jquery.*js',
-      'node_modules/bootstrap/dist/js/bootstrap*.js',
-      'node_modules/rxjs/bundles/Rx.js'
-    ]).pipe(gulp.dest('./wwwroot/libs/'));
+gulp.task("angular2:moveLibs", function () {
+    return gulp.src([
+        'node_modules/angular2/bundles/js',
+        'node_modules/angular2/bundles/angular2.*.js*',
+        'node_modules/angular2/bundles/angular2-polyfills.js',
+        'node_modules/angular2/bundles/http.*.js*',
+        'node_modules/angular2/bundles/router.*.js*',
+        'node_modules/es6-shim/es6-shim.min.js*',
+        'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
+        'node_modules/systemjs/dist/*.*',
+        'node_modules/rxjs/bundles/Rx.js'
+    ])
+        .pipe(gulp.dest(paths.webroot + "Angular"));
 
-    gulp.src([
-      'node_modules/bootstrap/dist/css/bootstrap.css'
-    ]).pipe(gulp.dest('./wwwroot/libs/css'));
 });
+
+gulp.task("angular2:moveJs", function () {
+    return gulp.src(["Angular/**/*.js"])
+        .pipe(gulp.dest(paths.webroot + "Angular/app/"));
+
+});
+gulp.task("angular2", ["angular2:moveLibs", "angular2:moveJs"])
---
+++ EF7EntityApp/src/EF7EntityApp/wwwroot/app/app.js
@@ -0,0 +1,6 @@
+"use strict";
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
+var browser_1 = require('angular2/platform/browser');
+var appComponent_1 = require('./appComponent');
+browser_1.bootstrap(appComponent_1.AppComponent);
+//# sourceMappingURL=app.js.map
